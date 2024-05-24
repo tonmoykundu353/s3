c6b661b3d7ec83b24dce3336619f95ac337fd647
Dipesh63 <134762024+Dipesh63@users.noreply.github.com>
2024-05-24 15:03:26 +0600

    Again

--- app/src/main/java/com/example/splash_learn/EventShowingAdapter.java
+++ app/src/main/java/com/example/splash_learn/EventShowingAdapter.java
@@ -6,95 +6,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
-import com.example.splash_learn.FlyweightDP.EventShowinginfo;
 
+import com.example.splash_learn.FlyweightDP.EventSharedProperties;
+import com.example.splash_learn.FlyweightDP.EventShowinginfo;
 
 import java.util.ArrayList;
 
-
-//
-//
-//public class EventShowingAdapter extends BaseAdapter {
-//    private Context context;
-//    private ArrayList<EventShowinginfo>arrayList;
-//
-//    public EventShowingAdapter(Context context, ArrayList<EventShowinginfo> arrayList) {
-//        this.context = context;
-//        this.arrayList = arrayList;
-//    }
-//
-//
-//
-//
-//    @Override
-//    public int getCount() {
-//        return arrayList.size();
-//    }
-//
-//    @Override
-//    public Object getItem(int position) {
-//        return arrayList.get(position);
-//    }
-//
-//    @Override
-//    public long getItemId(int position) {
-//        return position;
-//    }
-//
-//    @Override
-//    public View getView(int position, View convertView, ViewGroup parent) {
-//        if(convertView==null){
-//            convertView= LayoutInflater.from(context).inflate(R.layout.item_event_showing_layout,parent,false);
-//        }
-//        EventShowinginfo eventShowinginfo=arrayList.get(position);
-//        TextView clubnameview=convertView.findViewById(R.id.id_showing_club_name);
-//        TextView eventnameview=convertView.findViewById(R.id.id_showing_event_name);
-//        TextView eventplaceview=convertView.findViewById(R.id.id_showing_event_place);
-//        TextView eventregFeesview=convertView.findViewById(R.id.id_showing_event_regFees);
-//        TextView eventdateview=convertView.findViewById(R.id.id_showing_event_date);
-//
-//
-//
-//        clubnameview.setText(eventShowinginfo.getClubname());
-//        eventnameview.setText(eventShowinginfo.getEventname());
-//        eventplaceview.setText(eventShowinginfo.getEventplace());
-//        eventregFeesview.setText(eventShowinginfo.getEventRegFees());
-//        eventdateview.setText(eventShowinginfo.getEventdate());
-//
-//        return convertView;
-//
-//    }
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 public class EventShowingAdapter extends BaseAdapter {
     private Context context;
-    private ArrayList<EventShowinginfo>arrayList;
+    private ArrayList<EventShowinginfo> arrayList;
 
     public EventShowingAdapter(Context context, ArrayList<EventShowinginfo> arrayList) {
         this.context = context;
         this.arrayList = arrayList;
     }
-
-
-
 
     @Override
     public int getCount() {
@@ -113,25 +38,29 @@ public class EventShowingAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        if(convertView==null){
-            convertView= LayoutInflater.from(context).inflate(R.layout.item_event_showing_layout,parent,false);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.item_event_showing_layout, parent, false);
         }
-        EventShowinginfo eventShowinginfo=arrayList.get(position);
-        TextView clubnameview=convertView.findViewById(R.id.id_showing_club_name);
-        TextView eventnameview=convertView.findViewById(R.id.id_showing_event_name);
-        TextView eventplaceview=convertView.findViewById(R.id.id_showing_event_place);
-        TextView eventregFeesview=convertView.findViewById(R.id.id_showing_event_regFees);
-        TextView eventdateview=convertView.findViewById(R.id.id_showing_event_date);
-
-
+        EventShowinginfo eventShowinginfo = arrayList.get(position);
+        TextView clubnameview = convertView.findViewById(R.id.id_showing_club_name);
+        TextView eventnameview = convertView.findViewById(R.id.id_showing_event_name);
+        TextView eventplaceview = convertView.findViewById(R.id.id_showing_event_place);
+        TextView eventregFeesview = convertView.findViewById(R.id.id_showing_event_regFees);
+        TextView eventdateview = convertView.findViewById(R.id.id_showing_event_date);
 
         clubnameview.setText(eventShowinginfo.getClubname());
-        eventnameview.setText(eventShowinginfo.getEventName());
-        eventplaceview.setText(eventShowinginfo.getSharedProperties().getEventPlace());
+        eventnameview.setText(eventShowinginfo.getEventname());
         eventregFeesview.setText(eventShowinginfo.getEventRegFees());
-        eventdateview.setText(eventShowinginfo.getEventDate());
+        eventdateview.setText(eventShowinginfo.getEventdate());
+
+        // Use shared properties to set event place and organizer
+        EventSharedProperties sharedProperties = eventShowinginfo.getEventSharedProperties();
+        if (sharedProperties != null) {
+            eventplaceview.setText(sharedProperties.getEventPlace());
+            // You can set organizer wherever needed
+        }
 
         return convertView;
-
     }
 }
+
