f4b24a2d963762419d85eb9ae716dad2a4cf88de
unknown <byzdhmd24@gmail.com>
2024-05-24 19:33:02 +0600

    something commited

--- app/src/main/java/com/example/splash_learn/Contestentinfo.java
+++ app/src/main/java/com/example/splash_learn/Contestentinfo.java
@@ -34,58 +34,78 @@ package com.example.splash_learn;
 //    }
 //
 //    public void setCollegename(String collegename) {
-//        this.collegename = collegename;
-//    }
-//}
+//
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/**
+ * This is Contestentinfo class for having the names ,emails and contactNo
+ */
 
 public class Contestentinfo {
     String name,email,ContactNo;
 
+    /**
+     * Paramterized Constructor added
+     * @param name
+     * @param email
+     * @param ContactNo
+     */
     public Contestentinfo(String name, String email, String ContactNo) {
         this.name = name;
         this.email = email;
         this.ContactNo = ContactNo;
     }
 
+    /**
+     * Default Constructor
+     */
     public Contestentinfo() {
 
     }
 
+    /**
+     * This will get name
+     * @return
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * this will used to set name
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * This will used to get email
+     * @return
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * This will used to set email
+     * @param email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * This will get contact info
+     * @return
+     */
     public String getContactNo() {
         return ContactNo;
     }
 
+    /**
+     * This will be used to set contactNo
+     * @param ContactNo
+     */
     public void setContactNo(String ContactNo) {
         this.ContactNo = ContactNo;
     }
